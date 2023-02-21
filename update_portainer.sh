e34cb40898c67362173e6366c8349c7dba18335e
Toomas <52831344+Toomas633@users.noreply.github.com>
2023-02-21 11:47:07 +0200

    Update update_portainer.sh

--- update_portainer.sh
+++ update_portainer.sh
@@ -6,10 +6,10 @@ sudo docker pull portainer/portainer-ce:latest
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 read -n4 -p "Update portainer agent? (yes/no):" service_status
+
 case $service_status in
-  y|Y|yes|Yes|YES) systemctl enable xmrig.service ;; 
+  y|Y|yes|Yes|YES) sudo wget https://raw.githubusercontent.com/Toomas633/Installers/main/update_portainer_agent
+                   sudo chmod a+x update_portainer_agent.sh
+                   sudo ./update_portainer_agent.sh ;; 
   n|N|no|No|NO) echo Done! ;;
 esac
-
-sudo wget https://raw.githubusercontent.com/Toomas633/Installers/main/update_portainer_agent
-sudo chmod a+x 

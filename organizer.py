38f579954206a49335b7806d96c3b520b495a2eb
Toomas <toomas.kirsing@gmail.com>
2023-03-03 00:44:46 +0200

    Update organizer.py

--- organizer.py
+++ organizer.py
@@ -1,14 +1,15 @@
 import os
+import subprocess
 
-# root dir
-rootDir = './folder/'
-
-# loop through each directory in the tree
+rootDir = os.getcwd()
+# Command-line argument was provided
 for dirName, subdirList, fileList in os.walk(rootDir):
     # loop through each file
     for fname in fileList:
         # if the file is a certain type, keep it
-        if fname.endswith('.txt'):
+        if fname.endswith('.mkv') or fname.endswith('.mp4') or fname.endswith('.srt') or fname.endswith('.!qB'):
             continue
         # otherwise, delete it
         os.remove(os.path.join(dirName, fname))
+# delete empty folders in working dir
+subprocess.call('find . -type d -empty -delete', shell=True)

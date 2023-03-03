d46b92fc6094cd22837a8f54d04538d6b0d3a45c
Toomas <toomas.kirsing@gmail.com>
2023-03-03 22:50:18 +0200

    Update organizer.py

--- organizer.py
+++ organizer.py
@@ -7,7 +7,8 @@ dir = os.getcwd()
 # Write status to log file
 logging.basicConfig(filename='organizer.log', level=logging.INFO, format='%(asctime)s %(message)s')
     
-def organizer(rootDir):
+def remover(rootDir):
+    logging.info('Removing files in ' + rootDir)
     # Command-line argument was provided
     for dirName, subdirList, fileList in os.walk(rootDir):
         # loop through each file
@@ -18,7 +19,9 @@ def organizer(rootDir):
             # otherwise, delete it
             os.remove(os.path.join(dirName, fname))
             logging.info('File ' + fname + ' deleted in ' + rootDir)
-
+            
+def mover(rootDir):
+    logging.info('Moving files in ' + rootDir)
     # Walk through subdirectories
     for root, dirs, files in os.walk(rootDir):
         # Check if there is a .mkV file
@@ -35,21 +38,23 @@ def organizer(rootDir):
                             shutil.move(os.path.join(subdir_path, fname), root)
                             logging.info('Sub file ' + fname +  'moved in ' + rootDir)
 
-    # list the files and folders in the working directory
-    for filename in os.listdir(rootDir):
-        # check if the file/folder is a directory
-        if os.path.isdir(filename):
-            # check if the directory is empty
-            if not os.listdir(filename):
-                # if it is empty, delete it
-                os.rmdir(filename)
-                logging.info('Empty folder ' + filename + ' deleted in ' + rootDir)
+def empty(rootDir):
+    logging.info('Removing empty folders in' + rootDir)
+    # Walk through the path and delete empty subfolders
+    for root, dirs, files in os.walk(rootDir):
+        for dir in dirs:
+            full_path = os.path.join(root, dir)
+            if not os.listdir(full_path):
+                shutil.rmtree(full_path)
+                logging.info('Empty folder ' + dir + ' deleted in ' + rootDir)
 
 # set /movies as working dir and run
-logging.info('Organizing /movies dir')
 rootDir = dir + '/movies'
-organizer(rootDir)
+remover(rootDir)
+mover(rootDir)
+empty(rootDir)
 # set /tv as working dir and run
-logging.info('Organizing /tv dir')
 rootDir = dir + '/tv'
-organizer(rootDir)
+remover(rootDir)
+mover(rootDir)
+empty(rootDir)

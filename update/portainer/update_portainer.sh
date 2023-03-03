d2ffc010f40a56f0002770315fd509482a15bee7
Toomas <52831344+Toomas633@users.noreply.github.com>
2023-03-03 15:02:12 +0200

    Update update_portainer.sh

--- update/portainer/update_portainer.sh
+++ update/portainer/update_portainer.sh
@@ -8,7 +8,7 @@ sudo docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -
 read -n4 -p "Update portainer agent? (yes/no):" service_status
 
 case $service_status in
-  y|Y|yes|Yes|YES) sudo wget https://raw.githubusercontent.com/Toomas633/Installers/main/update_portainer_agent
+  y|Y|yes|Yes|YES) sudo wget https://raw.githubusercontent.com/Toomas633/Scripts/main/update/portainer/update_portainer_agent.sh
                    sudo chmod a+x update_portainer_agent.sh
                    sudo ./update_portainer_agent.sh ;; 
   n|N|no|No|NO) echo Done! ;;

4eec04c39a25bc8ff30b73af00ba79d8880ac8b5
Toomas <toomas.kirsing@gmail.com>
2023-08-03 18:12:08 +0300

    Move minecraft scripts to a seperate folder

--- minecraft/mc-background.py
+++ minecraft/mc-background.py
@@ -3,27 +3,27 @@ import time
 import subprocess
 import logging
 
+logging.basicConfig(filename='mc-background.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
+
 def get_player_count(server_address):
     try:
-        server = mcstatus.JavaServer.lookup(f"{server_address}")
+        server = mcstatus.JavaServer.lookup(server_address)
         status = server.status()
         player_count = status.players.online
         return player_count
     except Exception as e:
         logging.exception(e)
         return None
-    
+
 def start_service(service_name):
     try:
         subprocess.run(["sudo", "systemctl", "start", service_name], check=True)
-        print(f"{service_name} started successfully.")
     except subprocess.CalledProcessError:
         logging.error(f"Failed to start {service_name}")
 
 def stop_service(service_name):
     try:
         subprocess.run(["sudo", "systemctl", "stop", service_name], check=True)
-        print(f"{service_name} stopped successfully.")
     except subprocess.CalledProcessError:
         logging.error(f"Failed to stop {service_name}")
 
@@ -36,15 +36,14 @@ def is_service_running(service_name):
         return False
 
 if __name__ == "__main__":
-    server_address = "vanilla.toomas633.com"
-    service_name = "xmrig.service"
-    logging.basicConfig('organizer.log', level=logging.INFO, format='%(asctime)s %(message)s')
+    server_address = "ip:port or domain"
+    service_name = "example.service"
 
     while True:
         player_count = get_player_count(server_address)
         logging.info(f"Players online: {player_count}")
-        if player_count is not None:
-            if is_service_running(service_name) == "active":
+        if player_count > 0:
+            if is_service_running(service_name):
                 stop_service(service_name)
                 logging.info(f"Stopped {service_name}")
         else:
